2306c5c07548ff1876f3ead6790d6ee02da15bdf
hardik2000joshi <165023667+hardik2000joshi@users.noreply.github.com>
2024-12-13 12:36:21 +0530

    Print Hello through string

---
+++ Hello_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    char a[]="Hello";
+    char b[50]="Hello";
+    char c[]={'H', 'E', 'L', 'L', 'O'};
+    char d[20]={'H', 'E', 'L', 'L', 'O'};
+    printf("%s\n", a);
+    printf("%s\n", b);
+    printf("%s\n", c);
+    return 0;
+}
